9ce3bc6255faa85e34801f7956c8623517d715d8
Mike Mochan <mmochan@mac.com>
2018-08-08 19:00:09 +1000

    fallback

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -76,24 +76,7 @@ object Parseyaml : BuildType({
             scriptContent = "call gradlew.bat"
         }
         gradle {
-            buildscript {
-                repositories {
-                    mavenCentral()
-                }
-                dependencies {
-                    classpath group: 'org.yaml', name: 'snakeyaml', version: '1.19'
-                }
-            }
 
-            def cfg = new org.yaml.snakeyaml.Yaml().load( new File("cfg.yml").newInputStream() )
-
-            task testMe( ){
-                doLast {
-                    println "make "
-                    println "profile =  ${cfg.spring.profiles.active}"
-                    assert cfg.spring.profiles.active == "prod"
-                }
-            }
         }
     }
 })
